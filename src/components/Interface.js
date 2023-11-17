1b5dc10d25a5c0137ff1eb340100efcd008005d4
LucasSP23 <lucass.pereira2004@gmail.com>
2023-11-16 22:55:35 -0300

    telacriada

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,5 +1,15 @@
-const a = () => {
-    return (
-        <div>ola</div>
+const Interface = ({tr, remover,finalizar}) =>{
+    
+    return(
+        <div>
+            <p>EXERCICIO: {tr.exercicio}</p>
+            <p>REPETIÇÃO: {tr.repeticao}</p>
+            <p>SERIE: {tr.serie}</p>
+            <button className='botao'>ENCERRADA</button>
+            <button className='botao'>APAGAR</button>
+            <hr/>
+        </div>  
     )
 }
+
+export default Interface;
