aa05b9c47dfded3370a6c56293765074b50db097
Gabriel Oliveira <GabrielAdsCamp@gmail.com>
2023-11-16 21:44:00 -0300

    criando o inicio das funções

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
+import Inferiores from './pages/Inferiores'
+import Superiores from './pages/Superiores'
+import Sobre from './pages/Sobre'
+import Login from './pages/Login'
+import Cadastro from './pages/Cadastro'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+return (
+  <Router>
+    <Routes>
+      <Route path="/Inferiores" element={<Inferiores/>}/>
+      <Route exact path="/Superiores" element={<Superiores/>}/>
+      <Route path="/Sobre" element={<Sobre/>}/>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/Cadastro" element={<Cadastro/>}/>
+    </Routes>
+  </Router>
+);
+} 
 
 export default App;
